a5d2feb6c0860b212674632dbb209dcf9408bd2b
Balduin Landolt <balduin.landolt@hotmail.com>
2021-02-10 00:54:42 +0100

    Add crawler for IDs

--- crawler.py
+++ crawler.py
@@ -1,11 +1,23 @@
+from typing import List
 from numpy import empty
 import pandas as pd
 import requests
+import os
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from itertools import chain
+from time import time
 
+
+# Constants
+# ---------
 
 _coll_path = 'data/collections.csv'
+_id_path = 'data/ms_ids.csv'
 
+
+# Utlity Functions
+# ----------------
 
 def _get_soup(url: str, parser='xml') -> BeautifulSoup:
     """Get a BeautifulSoup object from a URL
@@ -22,7 +34,10 @@ def _get_soup(url: str, parser='xml') -> BeautifulSoup:
     return soup
     
 
-def get_collections(use_cache=True, cache=True) -> pd.DataFrame:
+# Crawl Collections
+# -----------------
+
+def get_collections(use_cache: bool=True, cache: bool=True) -> pd.DataFrame:
     """Load all collections from handrit.isself.
 
     The dataframe contains the following informations:
@@ -37,8 +52,8 @@ def get_collections(use_cache=True, cache=True) -> pd.DataFrame:
     Returns:
         pd.DataFrame: Data frame containing basic information on collections.
     """
-    if use_cache:
-        cols = _load_collections_from_cache()
+    if use_cache and os.path.exists(_coll_path):
+        cols = pd.read_csv(_coll_path)
         if cols is not None and not cols.empty:
             return cols
     cols = _load_collections()
@@ -47,13 +62,7 @@ def get_collections(use_cache=True, cache=True) -> pd.DataFrame:
     return cols
 
 
-def _load_collections_from_cache():
-    """Load collections from local cache
-    """
-    return pd.read_csv(_coll_path)
-
-
-def _load_collections():
+def _load_collections() -> pd.DataFrame:
     """Load collections from website
     """
     soup = _get_soup('https://handrit.is/#collection', 'lxml')
@@ -63,8 +72,55 @@ def _load_collections():
                     c.find('a', attrs={'class': 'viewall'})['href'].rsplit(';')[0] + '?showall.browser=1',) for c in collection_tags]
     df = pd.DataFrame(collections, columns=['collection', 'ms_count', 'url'])
     return df
+    
 
+# Crawl Manuscript IDs
+# -----------------
+
+def get_ids(urls: List[str], use_cache: bool=True, cache: bool=True, max_res: int=-1, aggressive_crawl: bool=True):
+    # print(*urls, sep='\n')
+    if use_cache and os.path.exists(_id_path):
+        ids = pd.read_csv(_id_path)
+        if ids is not None and not ids.empty:
+            return list(ids.id)
+    ids = _load_ids(urls, max_res=max_res, aggressive_crawl=aggressive_crawl)
+    if max_res > 0 and len(ids) >= max_res:
+        ids = ids[:max_res]
+    if cache:
+        df = pd.DataFrame({'id': ids})
+        df.to_csv(_id_path, encoding='utf-8', index=False)
+    return ids
+
+
+def _load_ids(urls: List[str], max_res: int=-1, aggressive_crawl: bool=False) -> pd.DataFrame:
+    if aggressive_crawl:
+        with ThreadPoolExecutor() as executor:
+            res = executor.map(_load_ids_from_url, urls)
+        return list(set(chain.from_iterable(res)))
+    else:
+        res = []
+        for url in urls:
+            res.extend(_load_ids_from_url(url))
+            if max_res > 0 and len(res) >= max_res:
+                break
+        return list(set(res))
+
+
+def _load_ids_from_url(url):
+    soup = _get_soup(url, 'lxml')
+    res = []
+    for td in soup.find_all('td', attrs={'class': 'id'}):
+        res.append(td.text)
+    return res
+
+
+# Test Runner
+# -----------
 
 if __name__ == "__main__":
     cols = get_collections()
-    print(cols)
+    # print(cols)
+    start = time()
+    get_ids(list(cols.url))
+    stop = time()
+    print(stop - start)
