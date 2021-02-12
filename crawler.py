7757c70d24db4040589a8361bf87e179f61b2a0d
Balduin Landolt <balduin.landolt@hotmail.com>
2021-02-12 02:02:33 +0100

    Improve ID crawling

--- crawler.py
+++ crawler.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Generator, List, Tuple
 from numpy import empty
 import pandas as pd
 import requests
@@ -16,6 +16,8 @@ from datetime import datetime
 _coll_path = 'data/collections.csv'
 _id_path = 'data/ms_ids.csv'
 _xml_url_path = 'data/xml_urls.csv'
+
+_backspace_print = '                                     \r'
 
 
 # Utlity Functions
@@ -40,7 +42,7 @@ def _get_soup(url: str, parser='xml') -> BeautifulSoup:
 # -----------------
 
 def get_collections(use_cache: bool=True, cache: bool=True) -> pd.DataFrame:
-    """Load all collections from handrit.isself.
+    """Load all collections from handrit.is.
 
     The dataframe contains the following informations:
     - Collection ID
@@ -77,48 +79,113 @@ def _load_collections() -> pd.DataFrame:
     
 
 # Crawl Manuscript IDs
-# -----------------
+# --------------------
 
-def get_ids(urls: List[str], use_cache: bool=True, cache: bool=True, max_res: int=-1, aggressive_crawl: bool=True) -> List[str]:
-    # TODO: Docstring
-    # print(*urls, sep='\n')
+def get_ids(df: pd.DataFrame, use_cache: bool=True, cache: bool=True, max_res: int=-1, aggressive_crawl: bool=True) -> pd.DataFrame:
+    """Load all manuscript IDs.
+
+    The dataframe contains the following collumns:
+    - Collection ID
+    - Manuscript ID
+
+    Args:
+        df (pd.DataFrame): Dataframe containing the available collections on handrit. (Cf. `get_collections()`)
+        use_cache (bool, optional): Flag true if local cache should be used; false to force download. Defaults to True.
+        cache (bool, optional): Flag true if result should be written to cache. Defaults to True.
+        max_res (int, optional): Maximum number of results to return (mostly for testing quickly). For unrestricted, use -1. Defaults to -1.
+        aggressive_crawl (bool, optional): Aggressive crawlin mode puts some strain on the server (and your bandwidth) but is much faster. Defaults to True.
+
+    Returns:
+        pd.DataFrame: Tataframe containing the manuscript IDs.
+    """
     if use_cache and os.path.exists(_id_path):
         ids = pd.read_csv(_id_path)
         if ids is not None and not ids.empty:
             return list(ids.id)
-    ids = _load_ids(urls, max_res=max_res, aggressive_crawl=aggressive_crawl)
-    if max_res > 0 and len(ids) >= max_res:
+    ids = _load_ids(df, max_res=max_res, aggressive_crawl=aggressive_crawl)
+    if max_res > 0 and len(ids.index) >= max_res:
         ids = ids[:max_res]
     if cache:
-        df = pd.DataFrame({'id': ids})
-        df.to_csv(_id_path, encoding='utf-8', index=False)
-    return list(ids)
+        ids.to_csv(_id_path, encoding='utf-8', index=False)
+    return ids
 
 
-def _load_ids(urls: List[str], max_res: int=-1, aggressive_crawl: bool=False) -> pd.DataFrame:
+def _load_ids(df: pd.DataFrame, max_res: int=-1, aggressive_crawl: bool=True) -> pd.DataFrame:
+    """Load IDs"""
     if aggressive_crawl:
-        with ThreadPoolExecutor() as executor:
-            res = executor.map(_load_ids_from_url, urls)
-        return list(set(chain.from_iterable(res)))
+        return _load_ids_aggressively(df, max_res)
     else:
-        res = []
-        for url in urls:
-            res.extend(_load_ids_from_url(url))
-            if max_res > 0 and len(res) >= max_res:
-                break
-        return list(set(res))
+        iterator_ = _load_ids_chillfully(df)
+        if max_res > 0:
+            res = pd.DataFrame(columns=['collection', 'id'])
+            for tuple_ in iterator_:
+                if len(res.index) >= max_res:
+                    return res
+                res = res.append({
+                    'collection': tuple_[0],
+                    'id': tuple_[1],
+                }, ignore_index=True)
+        else:
+            res = pd.DataFrame(iterator_, columns=['collection', 'id'])
+        return res
 
 
-def _load_ids_from_url(url):
+def _load_ids_aggressively(df: pd.DataFrame, max_res: int=-1):
+    """load IDs aggressively"""
+    iterator_ = _download_ids_aggressively(df, max_res)
+    if max_res > 0:
+        res = pd.DataFrame(columns=['collection', 'id'])
+        for tuple_ in iterator_:
+            if len(res.index) >= max_res:
+                return res
+            res = res.append({
+                'collection': tuple_[0],
+                'id': tuple_[1],
+            }, ignore_index=True)
+    else:
+        res = pd.DataFrame(iterator_, columns=['collection', 'id'])
+    return res.sort_values(by=['collection', 'id'], inplace=False)
+
+
+
+def _download_ids_aggressively(df: pd.DataFrame, max_res: int) -> Generator[Tuple[str], None, None]:
+    """Download IDs aggressively: launch Threads"""
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(_download_ids_from_url, s.url, s.collection) for _, s in df.iterrows()]
+        i = 0
+        for f in as_completed(futures):
+            for tup in f.result():
+                if max_res > 0 and i >= max_res:
+                    executor.shutdown(wait=False, cancel_futures=True)
+                    return
+                print(i, end='\r')
+                i += 1
+                yield tup
+        print('', end=_backspace_print)
+
+
+def _load_ids_chillfully(df: pd.DataFrame) -> Generator[Tuple[str], None, None]:
+    """Load IDs page by page"""
+    cols = list(df.collection)
+    for col in cols:
+        print(f'loading: {col}', end=_backspace_print)
+        url = df.loc[df.collection==col, 'url'].values[0]
+        for res in _download_ids_from_url(url, col):
+            yield res
+    print('', end=_backspace_print)
+
+
+def _download_ids_from_url(url: str, col: str) -> List[Tuple[str]]:
+    """get IDs from a collection URL"""
     soup = _get_soup(url, 'lxml')
     res = []
     for td in soup.find_all('td', attrs={'class': 'id'}):
-        res.append(td.text)
+        res.append((col, td.text))
     return res
     
 
-# Crawl Manuscript IDs
-# -----------------
+# Crawl XML URLs
+# --------------
 
 def get_xml_urls(ids: List[str], use_cache: bool=True, cache: bool=True, max_res: int=-1, aggressive_crawl: bool=True) -> List[str]:
     # TODO: Docstring
@@ -185,11 +252,10 @@ def _get_url_if_exists(url):
 
 if __name__ == "__main__":
     print(f'Start: {datetime.now()}')
-    cols = get_collections()
-    # print(cols)
-    ids = get_ids(list(cols.url))
     start = time()
-    xml_urls = get_xml_urls(ids, use_cache=False)
+    cols = get_collections()
+    ids = get_ids(cols, use_cache=False)
     stop = time()
+    # xml_urls = get_xml_urls(ids, use_cache=False)
     print(stop - start)
     print(f'Finished: {datetime.now()}')
