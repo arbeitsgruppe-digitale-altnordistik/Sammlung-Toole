f9ca2251b825957058b838e027cfd2afb9ed7732
kraus-s <sven.kraus@unibas.ch>
2021-03-10 14:33:21 +0100

    probably obsolete already

--- test.py
+++ test.py
@@ -14,12 +14,22 @@ for files in _xml_path.glob(('*.xml')):
         with open(files, encoding='UTF-8') as xml:
             soup = BeautifulSoup(xml, 'xml')
         _id = soup.msDesc['xml:id']
+        _shelf = soup.msDesc.idno.get_text()
+        _lang = soup.msDesc['xml:lang']
+        try:
+            postquem = soup.msDesc.origDate['notBefore']
+            antequem = soup.msDesc.origDate['notAfter']
+        except:
+            postquem = "N/A"
+            antequem = "N/A
     except:
         try:
             # import pdb; pdb.set_trace()
             with open(files, 'rb') as xml:
                 soup = BeautifulSoup(xml, features='xml', from_encoding='iso-8859-1')
             _id = soup.msDesc['xml:id']
+            _shelf = soup.msDesc.idno.get_text()
+            _lang = soup.msDesc['xml:lang']
         except:
             print("Gods dammit, handrit!")
     try:
@@ -28,13 +38,12 @@ for files in _xml_path.glob(('*.xml')):
     except:
         postquem = "N/A"
         antequem = "N/A"
-    _shelf = soup.msDesc.idno.get_text()
-    _lang = soup.msDesc['xml:lang']
+    
     resDict['XMLID'].append(_id)
     resDict['Shelfmark'].append(_shelf)
     resDict['Language'].append(_lang)
     resDict['Terminus Postquem'].append(postquem)
     resDict['Terminus Antequem'].append(antequem)
 
-
-
+datingPD = pd.DataFrame.from_dict(resDict)
+print(datingPD)
