2a18d921bf8486dcb865f43d1c80770a65e9136d
ElineElmiger <78200324+ElineElmiger@users.noreply.github.com>
2021-10-28 12:12:28 +0200

    Update guiUtils.py

--- gui/guiUtils.py
+++ gui/guiUtils.py
@@ -2,7 +2,7 @@ class Texts:
     class SearchPage:
         instructions = """
 How to use this function:
-Insert any handrit result URLs in the box below, separated by __commma__.
+Insert any handrit result URLs in the box below, separated by __comma__.
 Processing can take several seconds.
 """
     class HowToPage:
