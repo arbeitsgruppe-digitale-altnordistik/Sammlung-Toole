6e6aed511f6f3ffba0febeabd13a975ffb64e728
Balduin Landolt <balduin.landolt@hotmail.com>
2022-09-23 10:00:40 +0200

    streamline Home page

--- src/gui/Home.py
+++ src/gui/Home.py
@@ -1,19 +1,9 @@
 import streamlit as st
-from src.gui.gui_utils import get_log, get_state, get_handler
 from src.lib.constants import IMAGE_HOME
-from src.lib.utils import Settings
 
 
-def main_page() -> None:
-    '''Landing page'''
-    st.title("Welcome to Sammlung Toole")
-    st.write("The Menu on the left has all the options")
-    st.image(IMAGE_HOME)
-
-
-log = get_log()
-state = get_state()
-dataHandler = get_handler()
-settings = Settings.get_settings()
-
-main_page()
+st.title("Welcome to Sammlung Toole")
+st.write("The Menu on the left has all the options")
+st.image(IMAGE_HOME)
+st.markdown("For more information, check the help page, or view the " +
+            "[documentation](https://arbeitsgruppe-digitale-altnordistik.github.io/Sammlung-Toole/).")
