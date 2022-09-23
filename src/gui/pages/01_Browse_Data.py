736e4b8420ba690f2426bc4423a3cec23807fc51
Balduin Landolt <balduin.landolt@hotmail.com>
2022-09-23 10:08:39 +0200

    Update 01_Browse_Data.py

--- src/gui/pages/01_Browse_Data.py
+++ src/gui/pages/01_Browse_Data.py
@@ -18,17 +18,8 @@ with st.expander("Show all manuscripts"):
 txt = handler.texts
 st.header("Texts")
 st.write(f'Found {len(txt)} texts.')
-# st.dataframe(txt.head())
 with st.expander("Show all texts"):
     st.write(txt)
-# if st.button("Show text counts"):
-#     counts = txt.apply(
-#         lambda x: pd.Series({"count": x[x == True].count()})).transpose().sort_values(
-#         by=['count'],
-#         ascending=False).reset_index().rename(
-#         columns={"index": "text"})
-#     st.write(counts)
-# TODO: Implement again!
 
 # Persons
 pers = handler.get_all_ppl_data()
