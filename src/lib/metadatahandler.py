b2c7910851581ef3405ae33021bf9ac0f3ac0fef
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-10-04 21:38:22 +0200

    feat: add ms selection by shelfmark (#124)
    * add option to select manuscripts
    
    * catch potential errors
    
    * add group handling to select page

--- src/lib/metadatahandler.py
+++ src/lib/metadatahandler.py
@@ -21,12 +21,14 @@ def plot_date_scatter(metadata: pd.DataFrame) -> None:
 
 def plot_dims(metadata: pd.DataFrame) -> None:
     fig = utils.dimensions_plotting(metadata)
-    st.plotly_chart(fig, use_container_width=True)
+    if fig:
+        st.plotly_chart(fig, use_container_width=True)
 
 
 def plot_dims_facet(metadata: pd.DataFrame) -> None:
     fig = utils.dimensions_plotting_facet(metadata)
-    st.plotly_chart(fig, use_container_width=True)
+    if fig:
+        st.plotly_chart(fig, use_container_width=True)
 
 
 def process_ms_results(handler: DataHandler, mss: list[str]) -> None:
