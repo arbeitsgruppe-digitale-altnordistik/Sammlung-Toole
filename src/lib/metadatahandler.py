e59150e063c16882ff0185a2d9992a385ab62dbd
Balduin Landolt <balduin.landolt@hotmail.com>
2022-10-04 21:53:08 +0200

    fix: error while attempting to plot when no result was found

--- src/lib/metadatahandler.py
+++ src/lib/metadatahandler.py
@@ -15,17 +15,23 @@ def citavi_export(metadata: pd.DataFrame) -> None:
 
 
 def plot_date_scatter(metadata: pd.DataFrame) -> None:
+    if metadata.empty:
+        return
     fig = utils.date_plotting(metadata)
     st.plotly_chart(fig, use_container_width=True)
 
 
 def plot_dims(metadata: pd.DataFrame) -> None:
+    if metadata.empty:
+        return
     fig = utils.dimensions_plotting(metadata)
     if fig:
         st.plotly_chart(fig, use_container_width=True)
 
 
 def plot_dims_facet(metadata: pd.DataFrame) -> None:
+    if metadata.empty:
+        return
     fig = utils.dimensions_plotting_facet(metadata)
     if fig:
         st.plotly_chart(fig, use_container_width=True)
