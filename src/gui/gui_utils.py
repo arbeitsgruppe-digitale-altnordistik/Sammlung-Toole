4ef83b9daa3914da254884ad6c2f97a78aaedf54
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-01-23 20:53:37 +0100

    tidy up

--- src/gui/gui_utils.py
+++ src/gui/gui_utils.py
@@ -6,21 +6,21 @@ from src.lib.datahandler import DataHandler
 from src.lib.stateHandler import StateHandler
 
 
-@st.experimental_singleton   # type: ignore
+@st.experimental_singleton
 def get_log() -> Logger:
     return utils.get_logger("GUI")
 
 
-@st.experimental_singleton   # type: ignore
+@st.experimental_singleton
 def get_state() -> StateHandler:
     return StateHandler()
 
 
 def get_handler() -> DataHandler:
-    return _get_handler()   # type: ignore
+    return _get_handler()
 
 
-@st.experimental_singleton   # type: ignore
+@st.experimental_singleton
 def _get_handler() -> DataHandler:
     with st.spinner('Grabbing data handler...'):
         return DataHandler()
