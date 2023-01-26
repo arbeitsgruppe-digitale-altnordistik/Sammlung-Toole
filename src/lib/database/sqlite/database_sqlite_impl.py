29239f0e384cdd9401791b7355fc6ad8cd1c5b96
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2023-01-26 20:18:32 +0100

    unify sqlite models

--- src/lib/database/sqlite/database_sqlite_impl.py
+++ src/lib/database/sqlite/database_sqlite_impl.py
@@ -6,9 +6,8 @@ from sqlalchemy.future import Engine
 from sqlmodel import Session, SQLModel, col, create_engine, select
 
 from src.lib.constants import DATABASE_PATH_TMP
+from src.lib.database.sqlite.models import *
 from src.lib.groups import Group, GroupType
-from src.lib.models.data import *
-from src.lib.models.group import GroupDBModel
 
 
 def get_engine(db_path: str = DATABASE_PATH_TMP) -> Engine:
@@ -25,105 +24,89 @@ class DatabaseSQLiteImpl:
 
     def get_metadata(self, ms_ids: list[str]) -> pd.DataFrame:
         with Session(self.engine) as session:
-            statement = select(ManuscriptDBModel).where(col(ManuscriptDBModel.manuscript_id).in_(ms_ids))
+            statement = select(Manuscripts).where(col(Manuscripts.manuscript_id).in_(ms_ids))
             mss = session.exec(statement).all()
-            msdicts = [ms.dict() for ms in mss]
-            return pd.DataFrame(msdicts)
+            ms_dicts = [ms.dict() for ms in mss]
+            return pd.DataFrame(ms_dicts)
 
     # TODO: may be possible to improve performance with some kind of eager querying (goes for all those here)
     def ms_x_ppl(self, pers_ids: list[str]) -> list[str]:
         with Session(self.engine) as session:
-            statement = select(
-                ManuscriptDBModel.manuscript_id
-            ).where(
-                col(ManuscriptDBModel.people).any(col(PeopleDBModel.pers_id).in_(pers_ids))
-            )
+            statement = select(Manuscripts.manuscript_id).where(col(Manuscripts.people).any(col(People.pers_id).in_(pers_ids)))
             mss = session.exec(statement).all()
             return mss
 
     def ppl_x_mss(self, ms_ids: list[str]) -> list[str]:
         with Session(self.engine) as session:
-            statement = select(
-                PeopleDBModel.pers_id
-            ).where(
-                col(PeopleDBModel.manuscripts).any(col(ManuscriptDBModel.manuscript_id).in_(ms_ids))
-            )
+            statement = select(People.pers_id).where(col(People.manuscripts).any(col(Manuscripts.manuscript_id).in_(ms_ids)))
             mss = session.exec(statement).all()
             return mss
 
     def ms_x_txts(self, txts: list[str]) -> list[str]:
         with Session(self.engine) as session:
-            statement = select(
-                ManuscriptDBModel.manuscript_id
-            ).where(
-                col(ManuscriptDBModel.texts).any(col(TextDBModel.text_id).in_(txts))
-            )
+            statement = select(Manuscripts.manuscript_id).where(col(Manuscripts.texts).any(col(Texts.text_id).in_(txts)))
             mss = session.exec(statement).all()
             return mss
 
     def txts_x_ms(self, ms_ids: list[str]) -> list[str]:
         with Session(self.engine) as session:
-            statement = select(
-                TextDBModel.text_id
-            ).where(
-                col(TextDBModel.manuscripts).any(col(ManuscriptDBModel.manuscript_id).in_(ms_ids))
-            )
+            statement = select(Texts.text_id).where(col(Texts.manuscripts).any(col(Manuscripts.manuscript_id).in_(ms_ids)))
             mss = session.exec(statement).all()
             return mss
 
     def persons_lookup_dict(self) -> dict[str, str]:
         with Session(self.engine) as session:
-            ppl = session.exec(select(PeopleDBModel)).all()
+            ppl = session.exec(select(People)).all()
             res = {p.pers_id: f"{p.first_name} {p.last_name}" for p in ppl}
             return res
 
     def ms_lookup_dict(self) -> dict[str, list[str]]:
         with Session(self.engine) as session:
-            statement = select(ManuscriptDBModel.manuscript_id, ManuscriptDBModel.shelfmark, ManuscriptDBModel.title)
+            statement = select(Manuscripts.manuscript_id, Manuscripts.shelfmark, Manuscripts.title)
             mss = session.exec(statement).all()
             res = {x[0]: [x[1], x[2]] for x in mss}
             return res
 
     def txt_lookup_list(self) -> list[str]:
         with Session(self.engine) as session:
-            res = session.exec(select(TextDBModel.text_id)).all()
+            res = session.exec(select(Texts.text_id)).all()
             return res
 
     def get_ms_groups(self) -> list[Group]:
         with Session(self.engine) as session:
-            statement = select(GroupDBModel).where(GroupDBModel.group_type == GroupType.ManuscriptGroup)
+            statement = select(Groups).where(Groups.group_type == GroupType.ManuscriptGroup)
             groups = session.exec(statement)
             return [g.to_group() for g in groups]
 
     def get_ppl_groups(self) -> list[Group]:
         with Session(self.engine) as session:
-            statement = select(GroupDBModel).where(GroupDBModel.group_type == GroupType.PersonGroup)
+            statement = select(Groups).where(Groups.group_type == GroupType.PersonGroup)
             groups = session.exec(statement)
             return [g.to_group() for g in groups]
 
     def get_txt_groups(self) -> list[Group]:
         with Session(self.engine) as session:
-            statement = select(GroupDBModel).where(GroupDBModel.group_type == GroupType.TextGroup)
+            statement = select(Groups).where(Groups.group_type == GroupType.TextGroup)
             groups = session.exec(statement)
             return [g.to_group() for g in groups]
 
     def get_all_groups(self) -> list[Group]:
         with Session(self.engine) as session:
-            statement = select(GroupDBModel)
+            statement = select(Groups)
             groups = session.exec(statement)
             return [g.to_group() for g in groups]
 
     def add_group(self, group: Group) -> None:
         with Session(self.engine) as session:
-            db_model = GroupDBModel.make(group)
+            db_model = Groups.make(group)
             session.add(db_model)
             session.commit()
 
     def update_group(self, group: Group, group_id: UUID) -> None:
         with Session(self.engine) as session:
-            group_old = session.get(GroupDBModel, group_id)
+            group_old = session.get(Groups, group_id)
             if group_old is not None:
                 session.delete(group_old)
-            group_new = GroupDBModel.make(group)
+            group_new = Groups.make(group)
             session.add(group_new)
             session.commit()
---
+++ src/lib/database/sqlite/models.py
@@ -0,0 +1,135 @@
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from typing import Optional
+from uuid import UUID, uuid4
+
+from sqlmodel import Field, Relationship, SQLModel
+
+from src.lib.groups import Group, GroupType
+
+
+class Groups(SQLModel, table=True):
+    group_id: UUID = Field(primary_key=True, default_factory=uuid4)
+    group_type: GroupType
+    name: str
+    date: str
+    items: str
+
+    def to_group(self) -> Group:
+        data = self.dict()
+        data["date"] = datetime.fromtimestamp(float(self.date), timezone.utc).astimezone()
+        data["items"] = set(self.items.split("|")) if data["items"] else set()
+        return Group(**data)
+
+    @staticmethod
+    def make(group: Group) -> Groups:
+        data = {**group.__dict__}
+        data["date"] = group.date.timestamp()
+        data["items"] = '|'.join(group.items)
+        return Groups(**data)
+
+
+class PersonCatalogueJunction(SQLModel, table=True):
+    pers_id: Optional[str] = Field(default=None, foreign_key="people.pers_id", primary_key=True)
+    catalogue_id: Optional[str] = Field(
+        default=None,
+        foreign_key="catalogueentries.catalogue_id",
+        primary_key=True
+    )
+
+
+class PersonManuscriptJunction(SQLModel, table=True):
+    pers_id: Optional[str] = Field(default=None, foreign_key="people.pers_id", primary_key=True)
+    manuscript_id: Optional[str] = Field(
+        default=None,
+        foreign_key="manuscripts.manuscript_id",
+        primary_key=True
+    )
+
+
+class TextCatalogueJunction(SQLModel, table=True):
+    text_id: Optional[str] = Field(default=None, foreign_key="texts.text_id", primary_key=True)
+    catalogue_id: Optional[str] = Field(
+        default=None,
+        foreign_key="catalogueentries.catalogue_id",
+        primary_key=True
+    )
+
+
+class TextManuscriptJunction(SQLModel, table=True):
+    text_id: Optional[str] = Field(default=None, foreign_key="texts.text_id", primary_key=True)
+    catalogue_id: Optional[str] = Field(
+        default=None,
+        foreign_key="manuscripts.manuscript_id",
+        primary_key=True
+    )
+
+
+class Texts(SQLModel, table=True):
+    text_id: str = Field(primary_key=True)
+    catalogue_entries: list["CatalogueEntries"] = Relationship(back_populates="texts", link_model=TextCatalogueJunction)
+    manuscripts: list["Manuscripts"] = Relationship(back_populates="texts", link_model=TextManuscriptJunction)
+
+
+class People(SQLModel, table=True):
+    pers_id: str = Field(primary_key=True)
+    first_name: str
+    last_name: str
+    catalogue_entries: list["CatalogueEntries"] = Relationship(back_populates="people", link_model=PersonCatalogueJunction)
+    manuscripts: list["Manuscripts"] = Relationship(back_populates="people", link_model=PersonManuscriptJunction)
+
+
+class CatalogueEntries(SQLModel, table=True):
+    catalogue_id: str = Field(primary_key=True)
+    shelfmark: str
+    manuscript_id: str
+    catalogue_filename: str
+    title: str
+    description: str
+    date_string: str
+    terminus_post_quem: int
+    terminus_ante_quem: int
+    date_mean: int
+    dating_range: int
+    support: str
+    folio: int
+    height: str
+    width: str
+    extent: str
+    origin: str
+    creator: str
+    country: str
+    settlement: str
+    repository: str
+    texts: list[Texts] = Relationship(back_populates="catalogue_entries", link_model=TextCatalogueJunction)
+    people: list[People] = Relationship(back_populates="catalogue_entries", link_model=PersonCatalogueJunction)
+
+
+class Manuscripts(SQLModel, table=True):
+    manuscript_id: str = Field(primary_key=True)
+    shelfmark: str
+    catalogue_entries: int
+    catalogue_ids: str
+    catalogue_filenames: str
+    title: str
+    description: str
+    date_string: str
+    terminus_post_quem: int
+    termini_post_quos: str
+    terminus_ante_quem: int
+    termini_ante_quos: str
+    date_mean: int
+    date_standard_deviation: float
+    support: str
+    folio: int
+    height: str
+    width: str
+    extent: str
+    origin: str
+    creator: str
+    country: str
+    settlement: str
+    repository: str
+    texts: list[Texts] = Relationship(back_populates="manuscripts", link_model=TextManuscriptJunction)
+    people: list[People] = Relationship(back_populates="manuscripts", link_model=PersonManuscriptJunction)
